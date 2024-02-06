bc8d604589bf240407ad521d934f64d3581c952d
Zain Javaid <zainmfj@gmail.com>
2024-02-06 10:00:30 -0600

    Added deploy file

---
+++ rsync_deploy.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+from dotenv import load_dotenv
+
+
+load_dotenv('fistbump-web/config.env')
+
+def transfer_files(target_dir: str) -> None:
+    """Automatically transfer current toodoo code to external server via rsync
+
+
+    Args:
+        target_dir (str): Directory on remote server to write to
+    """
+    try:
+        server_address = os.environ['AWS_SERVER_ADDRESS']
+        key_path = os.environ['AWS_KEY_PATH']
+        curr_path = os.path.abspath(os.curdir)+'/'.replace('//', '/')
+        sudo_password = os.environ['password']
+        
+        command_root = f"echo '{sudo_password}' | sudo -S rsync -avz"
+        exclude_info = "--exclude='.git/' --exclude='test_client.py' --exclude='.DS_Store' --exclude='.gitignore' --exclude='__pycache__/' --exclude='rsync_deploy.py'"
+        rsync_info = f"-e 'ssh -i {key_path}' {curr_path} ubuntu@{server_address}:~/{target_dir}"
+        full_command = ' '.join([command_root, exclude_info, rsync_info])
+        
+        sp = subprocess.Popen(full_command, shell=True)
+        sp.wait()
+        sp.terminate()
+    except Exception as e:
+        print(e)
+        
+if __name__ == '__main__':
+    target_dir = 'fistbump_v1'
+    transfer_files(target_dir)
