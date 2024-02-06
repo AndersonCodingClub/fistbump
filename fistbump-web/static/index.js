caa2b7253b6ffacc6473958e11dabdd9f1f75074
Zain Javaid <zainmfj@gmail.com>
2024-02-05 22:02:06 -0600

    Modal content + working click events

--- fistbump-web/static/index.js
+++ fistbump-web/static/index.js
@@ -1,4 +1,5 @@
 const grayBackground = document.getElementById('grayed-out-background');
+const waitlistModal = document.getElementById('waitlist-modal');
 
 function openWaitlist() {
     grayBackground.style.display = 'block';
@@ -13,4 +14,14 @@ document.addEventListener('keydown', function(e) {
     if (e.key === 'Escape') {
         closeWaitlist();
     }
+});
+
+document.getElementById('grayed-out-background').addEventListener('click', function(e) {
+    if (e.target === this) {
+        closeWaitlist();
+    }
+});
+
+document.getElementById('waitlist-modal').addEventListener('click', function(e) {
+    e.stopPropagation();
 });
